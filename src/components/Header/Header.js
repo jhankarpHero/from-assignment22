8954d2824d38b545d63d7eb448ece3da0a8c8156
Programming Hero <programming.hero1@gmail.com>
2021-04-23 21:32:03 -0500

    a

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <h3 className="title">This is Head</h3>
+            <h3 className="title">This is Head. another change</h3>
         </div>
     );
 };
